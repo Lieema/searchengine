4fd952e7d953c0fe845afaa213436e2fb36c815c
Adrien Hellec <adrien.hellec@epita.fr>
2019-12-13 15:58:18 +0100

    Event bus : add loger and improve publish client choice

--- javaWeek/eventbus/src/main/java/entity/Broker.java
+++ javaWeek/eventbus/src/main/java/entity/Broker.java
@@ -3,10 +3,14 @@ package entity;
 import io.javalin.Javalin;
 import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsMessageContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 
 public class Broker {
+    public Logger logger = LogManager.getLogger(Broker.class);
+
     HashMap<String, Topic> topics;
     Javalin app;
 
@@ -24,10 +28,11 @@ public class Broker {
     }
 
     private void initBroadcastWS() {
+        logger.info("[BROADCAST] Init websocket");
         app.ws("/subscribe/broadcast/:topic", ws -> {
             ws.onConnect(ctx -> {
                 String topicName = ctx.pathParam("topic");
-                System.out.println("[BROADCAST][" + topicName + "] Client connected : " + ctx.getSessionId());
+                logger.info("[BROADCAST][" + topicName + "] Client connected : " + ctx.getSessionId());
                 if (!topics.containsKey(topicName))
                     topics.putIfAbsent(topicName, new Topic(topicName));
                 Topic topic = topics.get(topicName);
@@ -35,36 +40,44 @@ public class Broker {
             });
             ws.onMessage(ctx -> {
                 String topicName = ctx.pathParam("topic");
-                System.out.println("[BROADCAST][" + topicName + "] Got message from client : " + ctx.getSessionId());
-                if (!topics.containsKey(topicName))
+                logger.info("[BROADCAST][" + topicName + "] Got message from client : " + ctx.getSessionId());
+                if (!topics.containsKey(topicName)) {
+                    logger.debug("[BROADCAST][" + topicName + "] Create topic");
                     topics.putIfAbsent(topicName, new Topic(topicName));
+                }
                 Topic topic = topics.get(topicName);
+                logger.debug("[BROADCAST][" + topicName + "] Add client " + ctx.getSessionId() + " to topic");
                 topic.broadcastQueue.add(ctx);
             });
             ws.onClose(ctx -> {
-                System.out.println("[BROADCAST][" + ctx.pathParam("topic") + "] Client disconnected : " + ctx.getSessionId());
+                logger.info("[BROADCAST][" + ctx.pathParam("topic") + "] Client disconnected : " + ctx.getSessionId());
                 String topicName = ctx.pathParam("topic");
                 if (topics.containsKey(topicName)) {
                     Topic topic = topics.get(topicName);
+                    logger.debug("[BROADCAST][" + topicName + "] Remove client " + ctx.getSessionId() + " from topic");
                     topic.clients.remove(ctx);
                 }
             });
-            ws.onError(ctx -> System.out.println("Client error"));
+            ws.onError(ctx -> logger.error("[BROADCAST] Client error : " + ctx.getSessionId()));
         });
     }
 
     private void initPublishWS() {
+        logger.info("[PUBLISH] Init websocket");
         app.ws("/subscribe/publish/:topic", ws -> {
             ws.onConnect(ctx -> {
                 String topicName = ctx.pathParam("topic");
-                System.out.println("[PUBLISH][" + topicName + "] Client connected : " + ctx.getSessionId());
-                if (!topics.containsKey(topicName))
+                logger.info("[PUBLISH][" + topicName + "] Client connected : " + ctx.getSessionId());
+                if (!topics.containsKey(topicName)) {
+                    logger.debug("[PUBLISH][" + topicName + "] Create topic");
                     topics.putIfAbsent(topicName, new Topic(topicName));
+                }
                 Topic topic = topics.get(topicName);
+                logger.debug("[PUBLISH][" + topicName + "] Add client " + ctx.getSessionId() + " to topic");
                 topic.clients.add(ctx);
             });
             ws.onMessage(ctx -> {
-                System.out.println("[PUBLISH][" + ctx.pathParam("topic") + "] Got message from client : " + ctx.getSessionId());
+                logger.info("[PUBLISH][" + ctx.pathParam("topic") + "] Got message from client : " + ctx.getSessionId());
                 String topicName = ctx.pathParam("topic");
                 if (!topics.containsKey(topicName))
                     topics.putIfAbsent(topicName, new Topic(topicName));
@@ -72,42 +85,60 @@ public class Broker {
                 topic.publishQueue.add(ctx);
             });
             ws.onClose(ctx -> {
-                System.out.println("[PUBLISH][" + ctx.pathParam("topic") + "] Client disconnected : " + ctx.getSessionId());
+                logger.info("[PUBLISH][" + ctx.pathParam("topic") + "] Client disconnected : " + ctx.getSessionId());
                 String topicName = ctx.pathParam("topic");
                 if (topics.containsKey(topicName)) {
                     Topic topic = topics.get(topicName);
+                    logger.debug("[PUBLISH][" + topicName + "] Remove client " + ctx.getSessionId() + " from topic");
                     topic.clients.remove(ctx);
                 }
             });
-            ws.onError(ctx -> System.out.println("Client error"));
+            ws.onError(ctx -> logger.error("[PUBLISH] Client error : " + ctx.getSessionId()));
         });
     }
 
     private void brokerEvents() throws InterruptedException {
+        logger.info("[BROKER] Start event loop");
+        int publishIndex = 0;
+        boolean allQueuesEmpty;
         while (true) {
+            allQueuesEmpty = true;
             for (Topic t : topics.values()) {
                 if (t.broadcastQueue.size() > 0) {
                     WsMessageContext ctx = t.broadcastQueue.pop();
                     for (WsConnectContext client : t.clients)
                         if (!ctx.getSessionId().equals(client.getSessionId())) {
-                            System.out.println("[BROKER][" + t.name + "] Broadcast message from " + ctx.getSessionId() + " : " + ctx.message());
+                            logger.info("[BROKER][" + t.name + "] Broadcast message from " + ctx.getSessionId());
+                            logger.debug("[BROKER][" + t.name + "] message : " + ctx.message());
                             client.send(ctx.message());
                         }
+                    if (t.broadcastQueue.size() > 0)
+                        allQueuesEmpty = false;
                 }
+                else
+                    logger.trace("[BROKER] No messages in broadcastQueue");
                 if (t.publishQueue.size() > 0) {
                     WsMessageContext ctx = t.publishQueue.pop();
-                    WsConnectContext client = t.clients.pop();
-                    t.clients.add(client);
+                    WsConnectContext client = t.clients.get(publishIndex);
+                    publishIndex = (publishIndex + 1) % t.clients.size();
                     if (!ctx.getSessionId().equals(client.getSessionId())) {
-                        System.out.println("[BROKER][" + t.name + "] Publish message from " + ctx.getSessionId() + " : " + ctx.message());
+                        logger.info("[BROKER][" + t.name + "] Publish message from " + ctx.getSessionId());
+                        logger.debug("[BROKER][" + t.name + "] message : " + ctx.message());
                         client.send(ctx.message());
                     }
                     else if (t.clients.size() > 1){
                         t.publishQueue.push(ctx);
                     }
+                    if (t.publishQueue.size() > 0)
+                        allQueuesEmpty = false;
                 }
+                else
+                    logger.trace("[BROKER] No messages in publishQueue");
             }
-            Thread.sleep(1000);
+            if (allQueuesEmpty) {
+                logger.trace("[BROKER] Waiting 1 second and retrying.");
+                Thread.sleep(1000);
+            }
         }
     }
 }
--- javaWeek/eventbus/src/main/java/entity/Topic.java
+++ javaWeek/eventbus/src/main/java/entity/Topic.java
@@ -4,13 +4,15 @@ import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsMessageContext;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 
 public class Topic {
     public String name;
     public Deque<WsMessageContext> broadcastQueue = new ArrayDeque<>();
     public Deque<WsMessageContext> publishQueue = new ArrayDeque<>();
-    public Deque<WsConnectContext> clients = new ArrayDeque<>();
+    public List<WsConnectContext> clients = new ArrayList<>();
 
     public Topic(String name) {
         this.name = name;
