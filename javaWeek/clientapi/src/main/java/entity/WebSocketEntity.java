31aa4ed0e18f3c9abf719e6dd26b1c0a629687ea
Lieema <axel.vigny@epita.fr>
2019-12-13 12:55:05 +0100

    add interface EventBusCommunication

--- javaWeek/clientapi/src/main/java/entity/WebSocketEntity.java
+++ javaWeek/clientapi/src/main/java/entity/WebSocketEntity.java
@@ -1,37 +1,62 @@
 package entity;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import helper.ConvertJsonStringObject;
 import model.Message;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft;
 import org.java_websocket.handshake.ServerHandshake;
 
+import java.io.IOException;
 import java.net.URI;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class WebSocketEntity extends WebSocketClient {
-    public WebSocketEntity(URI serverUri, Draft draft) {
-        super(serverUri, draft);
+    private Queue<String> messageReceivedQueue;
+    private Message message;
+
+    public WebSocketEntity(URI serverUri) {
+        super(serverUri);
     }
 
-    public WebSocketEntity(URI serverURI) {
+    public WebSocketEntity(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+        messageReceivedQueue = new LinkedList<String>();
+    }
+
+    public WebSocketEntity(URI serverURI, String senderType, String messageType) {
         super(serverURI);
+        messageReceivedQueue = new LinkedList<String>();
+        this.message = new Message();
+        this.message.senderType = senderType;
+        this.message.messageType = messageType;
     }
 
     @Override
     public void onOpen(ServerHandshake handshakedata) {
+        ConvertJsonStringObject converter = new ConvertJsonStringObject();
+        send(converter.convertToJsonString(this.message));
     }
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
+        send("closed");
     }
 
     @Override
     public void onMessage(String message) {
+        messageReceivedQueue.add(message);
     }
 
     @Override
     public void onMessage(ByteBuffer message) {
+        String v = StandardCharsets.UTF_8.decode(message).toString();
+        messageReceivedQueue.add(v);
     }
 
     @Override
@@ -39,4 +64,15 @@ public class WebSocketEntity extends WebSocketClient {
         System.err.println("an error occurred:" + ex);
     }
 
+    public Queue<String> getMessageQueue() {
+        return messageReceivedQueue;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
 }
--- javaWeek/clientapi/src/main/java/service/EventBusCommunication.java
+++ javaWeek/clientapi/src/main/java/service/EventBusCommunication.java
@@ -1,13 +1,44 @@
 package service;
 
 
+import entity.WebSocketEntity;
+import helper.ConvertJsonStringObject;
+import model.Message;
+
 import java.net.URI;
 
 public abstract class EventBusCommunication {
+    WebSocketEntity webSocketEntity;
 
     EventBusCommunication(URI uri) {
+        webSocketEntity = new WebSocketEntity(uri);
     }
 
-    public void subscribe() {
+    public void subscribe(String senderType) {
+        Message toSend = new Message();
+        toSend.senderType = senderType;
+        toSend.messageType = "subscribe";
+        ConvertJsonStringObject converter = new ConvertJsonStringObject();
+
+        webSocketEntity.send(converter.convertToJsonString(toSend));
+    }
+
+    public void sendMessage(String messageType, String senderType, String message) {
+        Message toSend = new Message();
+        toSend.senderType = senderType;
+        toSend.messageType = messageType;
+        toSend.message = message;
+        ConvertJsonStringObject converter = new ConvertJsonStringObject();
+
+        webSocketEntity.send(converter.convertToJsonString(toSend));
+    }
+
+    public void unsubscribe(String senderType) {
+        Message toSend = new Message();
+        toSend.senderType = senderType;
+        toSend.messageType = "unsubscribe";
+        ConvertJsonStringObject converter = new ConvertJsonStringObject();
+
+        webSocketEntity.send(converter.convertToJsonString(toSend));
     }
 }
