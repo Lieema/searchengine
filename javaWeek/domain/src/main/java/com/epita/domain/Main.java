db95129743ee7cb7fb45b1e8ceb1e60f71e64a24
AArzop <37124191+AArzop@users.noreply.github.com>
2019-12-14 12:39:26 +0100

    Create Main.java

---
+++ javaWeek/domain/src/main/java/com/epita/domain/Main.java
@@ -0,0 +1,10 @@
+package com.epita.domain;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Domain domain = new Domain();
+
+        domain.startEventLoop();
+    }
+}
