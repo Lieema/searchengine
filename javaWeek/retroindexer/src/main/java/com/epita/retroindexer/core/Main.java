c61236a44bab37c488eb3a7efc2d3a86080c295c
AArzop <37124191+AArzop@users.noreply.github.com>
2019-12-14 16:58:30 +0100

    Add hiver on retroindex

--- javaWeek/retroindexer/src/main/java/com/epita/retroindexer/core/Main.java
+++ javaWeek/retroindexer/src/main/java/com/epita/retroindexer/core/Main.java
@@ -3,6 +3,7 @@ package com.epita.retroindexer.core;
 import com.epita.retroindexer.service.RestApi;
 import com.epita.retroindexer.service.WSCommunication;
 import com.mti.hivers.impl.Hivers;
+import com.mti.hivers.impl.provider.Singleton;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,9 +17,13 @@ public class Main {
 
         Hivers hiver = new Hivers();
 
-        RestApi api = new RestApi(8000);
+        hiver.addProvider(new Singleton<>(RestApi.class, new RestApi(8000)));
+
         try {
             WSCommunication ws = new WSCommunication(new URI("ws://localhost:8080/subscribe/broadcast/index_result_event"));
+            hiver.addProvider(new Singleton<>(WSCommunication.class, ws));
+            hiver.instanceOf(WSCommunication.class).get().startWS();
+
         } catch (URISyntaxException e) {
             logger.error("[MAIN] Error parsing URI");
         }
